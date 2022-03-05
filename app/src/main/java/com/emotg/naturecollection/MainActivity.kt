6bc160d7847ca4e70e664a2ce4052ec9d188c09c
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-05 22:48:33 +0200

    HomeFragment injection in fragment container

--- app/src/main/java/com/emotg/naturecollection/MainActivity.kt
+++ app/src/main/java/com/emotg/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package com.emotg.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emotg.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Injecter le fragment dans notre bpîte (fragment_container) */
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
