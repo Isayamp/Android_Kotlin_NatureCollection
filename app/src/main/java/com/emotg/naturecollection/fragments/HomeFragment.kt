dbdaa68adcdcc168a2f3a5270d7d13cc96bde00f
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-06 23:48:30 +0200

    update: recuperate the RecyclerView

--- app/src/main/java/com/emotg/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/com/emotg/naturecollection/fragments/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.emotg.naturecollection.R
+import com.emotg.naturecollection.adapter.PlantAdapter
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +16,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+
+        // Récuperer le RecyclerView
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        horizontalRecyclerView.adapter = PlantAdapter()
+
+        return view
     }
 }
