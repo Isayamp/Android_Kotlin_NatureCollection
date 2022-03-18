f785765cb4e3ca2f7445859d69bff43830745214
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-18 20:42:32 +0200

    impoer PlantItemDecoration class

--- app/src/main/java/com/emotg/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/com/emotg/naturecollection/fragments/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.emotg.naturecollection.R
 import com.emotg.naturecollection.adapter.PlantAdapter
+import com.emotg.naturecollection.adapter.PlantItemDecoration
 
 class HomeFragment : Fragment() {
 
@@ -25,6 +26,9 @@ class HomeFragment : Fragment() {
         // Récuperer le secong RecyclerView
         val verticalRecyclerView =  view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
         verticalRecyclerView.adapter = PlantAdapter(R.layout.item_vertical_plant)
+
+        // Margin entre les dernières plantes
+        verticalRecyclerView.addItemDecoration(PlantItemDecoration())
 
         return view
     }
