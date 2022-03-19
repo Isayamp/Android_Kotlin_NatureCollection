d91918efd23ded8b7938098326a236003fe5e58d
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-19 09:57:59 +0200

    create plant properties

--- app/src/main/java/com/emotg/naturecollection/PlantModel.kt
+++ app/src/main/java/com/emotg/naturecollection/PlantModel.kt
@@ -1,4 +1,10 @@
 package com.emotg.naturecollection
 
-class PlantModel {
-}
+class PlantModel(
+
+    // Création des propiétés d'une plante
+    val neme: String = "Tulipe",
+    val description: String = "Petite déscriotpn",
+    val imageUrl: String = "https://cdn.pixabay.com/photo/2020/01/18/21/57/lotus-4776450_960_720.png",
+    var liked: Boolean = false
+)
