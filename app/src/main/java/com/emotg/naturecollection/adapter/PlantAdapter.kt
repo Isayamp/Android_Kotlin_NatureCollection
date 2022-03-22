52e0d81b267f0ee051a07179d2bae1e498ece64f
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-22 17:11:22 +0200

    add context MainActivity in PlantAdapter arguments

--- app/src/main/java/com/emotg/naturecollection/adapter/PlantAdapter.kt
+++ app/src/main/java/com/emotg/naturecollection/adapter/PlantAdapter.kt
@@ -5,10 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideExtension
+import com.bumptech.glide.annotation.GlideModule
+import com.emotg.naturecollection.MainActivity
 import com.emotg.naturecollection.PlantModel
 import com.emotg.naturecollection.R
 
 class PlantAdapter(
+    private val context: MainActivity,
     private val plantList: List<PlantModel>,
     private val layoutId: Int
     ) : RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
@@ -30,6 +35,9 @@ class PlantAdapter(
 
         // recuperer les informations de la plante
         val currentPlant = plantList[position]
+
+        // utiliser Glide pour récuperer l'image à partir de son lien vers le composant
+        Glide.with(context)
     }
 
     override fun getItemCount(): Int = plantList.size
