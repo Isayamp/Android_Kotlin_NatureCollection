a1c859582411373c446ce56300ae9bc15d7e9bf2
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-25 22:41:05 +0200

    import FirebaseDatabase package

--- app/src/main/java/com/emotg/naturecollection/PlantRepository.kt
+++ app/src/main/java/com/emotg/naturecollection/PlantRepository.kt
@@ -1,4 +1,9 @@
 package com.emotg.naturecollection
 
+import com.google.firebase.database.FirebaseDatabase
+
 class PlantRepository {
+
+    // Se connecter à la refference "plants"
+    val databaseRef = FirebaseDatabase.getInstance().getReference("plants")
 }
