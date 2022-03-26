811a5c78c0c377f3064b2815b9737bd14e24328b
Isaya M Pombo <Isayamlelapombo@gmail.com>
2022-03-26 12:18:59 +0200

    create Singleton object

--- app/src/main/java/com/emotg/naturecollection/PlantRepository.kt
+++ app/src/main/java/com/emotg/naturecollection/PlantRepository.kt
@@ -4,10 +4,12 @@ import com.google.firebase.database.FirebaseDatabase
 
 class PlantRepository {
 
-    // Se connecter à la refference "plants"
-    val databaseRef = FirebaseDatabase.getInstance().getReference("plants")
-    
-    // creer une liste qui va contenir nos plante
-    val plantList = arrayListOf<PlantModel>()
+    object Signleton {
+        // Se connecter à la refference "plants"
+        val databaseRef = FirebaseDatabase.getInstance().getReference("plants")
+
+        // creer une liste qui va contenir nos plante
+        val plantList = arrayListOf<PlantModel>()
+    }
 
 }
